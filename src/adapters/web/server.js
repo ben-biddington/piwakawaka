3d85683cf84110e7a390762286877b10d84b26a8
Ben Biddington <ben.biddington@xero.com>
2019-03-17 09:55:03 +1300

    Revert "Add basic map"
    This reverts commit 089a4e7e14c690083acd380d39c16036851949d3.
    
    Locations are on stops only.

--- src/adapters/web/server.js
+++ src/adapters/web/server.js
@@ -13,8 +13,6 @@ const port = 1080;
 var express = require('express')
 var app = express()
 
-const googleMapsApiKey = process.env.GOOGLE_MAPS_API_KEY;
-
 app.get(/^\/api/, async (req, res) => {
   const url = `https://www.metlink.org.nz/${req.path}`;
   
@@ -41,13 +39,7 @@ app.get(/^\/(vanilla|dist)/, function (req, res) {
 
   console.log(`returning file <${path}>`);
 
-  const fs = require("fs");
-
-  fs.readFile(path, "utf8", function(err, data) {
-    if (err) throw err;
-    res.write(data.replace('GOOGLE_MAPS_API_KEY', googleMapsApiKey));
-    res.end();
-  });
+  res.sendFile(path);
 });
 
 app.listen(port, () => console.log(`Server running on ${port}...`));
