efbab61a3a9190ebcc870777e50991dd4d063dfe
Ben Biddington <ben.biddington@xero.com>
2019-03-22 17:12:22 +1300

    How to eval functions

--- test/unit.tests/about-cloning-test.js
+++ test/unit.tests/about-cloning-test.js
@@ -14,7 +14,7 @@ class Person extends Cloneable {
   }
 }
 
-describe('that it preserves values', () => {
+describe('that cloning preserves values', () => {
   it('for example', () => {
     var tim       = new Person("Tim", 11);
     var olderTim  = tim.aged(12);
@@ -22,5 +22,41 @@ describe('that it preserves values', () => {
     expect(tim.name).to.equal("Tim");
     expect(olderTim.name).to.equal("Tim");
     expect(olderTim.age).to.equal(12);
+  });
+});
+
+describe('Eval-ing', () => { 
+  it('works with functions', () => { 
+    const result = eval("1 + 1");
+
+    expect(result).to.equal(2);
+  });
+
+  it('works with serialized functions', () => { 
+    const fun = () => 1 + 1;
+    
+    const result = eval(fun.toString())();
+
+    expect(result).to.equal(2);
+  });
+
+  it('works with function ctor', () => { 
+    const fun = () => 1 + 1;
+    
+    const func = new Function("return 1+1");
+
+    const result = func();
+
+    expect(result).to.equal(2);
+  });
+
+  it('does not work with serialized function', () => { 
+    const fun = () => 1 + 1;
+    
+    const func = new Function(fun.toString());
+
+    const result = func();
+
+    expect(result).to.equal(2);
   });
 });
