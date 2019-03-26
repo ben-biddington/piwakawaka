6260b81d4fff731786650520df2ba918e2646cf7
Ben Biddington <ben.biddington@gmail.com>
2019-03-27 09:52:42 +1300

    Enable `bash bus stops`

--- src/adapters/metlink-cli.js
+++ src/adapters/metlink-cli.js
@@ -5,6 +5,56 @@ const log       = m => fs.writeSync(1, `${m}\n`);
 
 const program = require('commander');
 
+const updateStops = (log, stopNumber) => {
+  const fs        = require('fs');
+
+  const { promisify } = require('util');
+  const readFile      = promisify(fs.readFile);
+  const writeFile     = promisify(fs.writeFile);
+  const exists        = promisify(fs.exists);
+
+  const fileName = "./.stops"; 
+
+  return exists(fileName).
+    then(fileExists => {
+      if (false === fileExists)
+        return writeFile("./.stops", '[]', "utf8");
+       
+      return Promise.resolve();
+    }).
+    then(()   => readFile(fileName, "utf8")).
+    then(text => {
+      const stops = JSON.parse(text || "[]");
+      
+      if (stops.filter(stop => stop == stopNumber).length === 0) {
+        stops.push(stopNumber);
+      }
+      
+      return stops;
+    }).
+    then(stops => writeFile("./.stops", JSON.stringify(stops), "utf8"));
+}
+
+const listStops = log => {
+  const fs        = require('fs');
+
+  const { promisify } = require('util');
+  const readFile      = promisify(fs.readFile);
+  const exists        = promisify(fs.exists);
+
+  const fileName = "./.stops"; 
+
+  return exists(fileName).
+    then(fileExists => {
+      if (false === fileExists)
+        return writeFile("./.stops", '[]', "utf8");
+       
+      return Promise.resolve();
+    }).
+    then(()   => readFile(fileName, "utf8")).
+    then(text => log(JSON.parse(text || "[]").join('\n')));
+}
+
 program.
   version('0.0.1').
   command("due <stopNumber> [routeNumber...]").
@@ -25,7 +75,13 @@ program.
     
     const debug     = (process.env.DEBUG == 1 || cmd.verbose === true) ? m => fs.writeSync(1, `[DEBUG] ${m}\n`) : _ => {};
 
-    return run({ log, debug, get }, opts);
+    return Promise.all([updateStops(log, stopNumber), run({ log, debug, get }, opts)]);
+  });
+
+program.
+  command("stops").
+  action(cmd => {
+    listStops(log);
   });
 
 program.parse(process.argv);
