6da737b01dfee2794fe9faf9dad2be7c7078592b
Ben Biddington <ben.biddington@gmail.com>
2019-03-27 16:57:57 +1300

    [GREEN] Can find bus stop info

--- src/adapters/metlink.js
+++ src/adapters/metlink.js
@@ -1,3 +1,11 @@
+const parse = (text) => {
+  try {
+    return JSON.parse(text);
+  } catch (error) {
+    throw `Failed to parse this text to json:\n\n${JSON.stringify(text)}`;
+  }
+}
+
 const realTime = async (ports = {}, opts = {}) => { 
   const { get, log = _ => {} } = ports;
   const { baseUrl = 'https://www.metlink.org.nz/api/v1/StopDepartures', stopNumber, enableDebug = false } = opts;
@@ -20,14 +28,6 @@ const realTime = async (ports = {}, opts = {}) => {
 
   debug(`URL: ${url}, routeNumbers: ${routeNumbers}`);
 
-  const parse = (text) => {
-    try {
-      return JSON.parse(text);
-    } catch (error) {
-      throw `Failed to parse this text to json:\n\n${JSON.stringify(text)}`;
-    }
-  }
-
   const reply = await get(url, {}).
     then(reply => parse(reply)).
     then(reply => { debug(`Full reply from <${url}>:\n${JSON.stringify(reply, null, 2)}`); return reply; });
@@ -48,4 +48,33 @@ const realTime = async (ports = {}, opts = {}) => {
   return { stop: { name: reply.Stop.Name, sms: reply.Stop.Sms}, arrivals };
 }
 
+const stops = async (ports = {}, opts = {}, ...stopNumbers) => {
+  const { get, log = _ => {} } = ports;
+  const { baseUrl = 'https://www.metlink.org.nz/api/v1/Stop', enableDebug = false } = opts;
+  
+  const debug = enableDebug === true ? (m) => log(`[DEBUG] ${m}`) : _ => {};
+
+  if (!get)
+    throw "You need to supply ports with a `get` function";
+
+  const result = await Promise.all(stopNumbers.map(stopNumber => {
+    const url  = `${baseUrl}/${stopNumber}`;
+    
+    debug(`URL: ${url}, stopNumbers: ${stopNumbers}`);
+    
+    return get(url, {}).
+      then(reply => parse(reply)).
+      then(reply => { debug(`Full reply from <${url}>:\n${JSON.stringify(reply, null, 2)}`); return reply; }).
+      then(thirdPartyStop => ({
+        name: thirdPartyStop.Name,
+        sms : thirdPartyStop.Sms
+      }));
+  }));
+
+  debug(result);
+
+  return result;
+}
+
 module.exports.realTime = realTime;
+module.exports.stops    = stops;
--- test/integration.tests/metlink/listing-stop-examples.js
+++ test/integration.tests/metlink/listing-stop-examples.js
@@ -1,15 +1,20 @@
 const expect      = require('chai').expect;
 const settings    = require('../../acceptance.tests/support/settings');
-const stops    = require('../../../src/adapters/metlink').stops;
+const stops       = require('../../../src/adapters/metlink').stops;
 const enableDebug = settings.features.enableDebug;
+const { cannedGet } = require('../support/net');
 
 describe('Querying for bus stops', () => {
-  xit('can find stops by id', async () => {
-    const ports = { get: fakeGet, log: settings.log };
+  it('can find stops by id', async () => {
+    const jsonText = '{"Name":"Manners Street at Cuba Street - Stop A","Sms":"5515","Farezone":"1","Lat":-41.2914083,"Long":174.7771028,"LastModified":"2019-03-27T00:01:07+13:00"}';
     
-    const result = await stops(ports, '4130');
+    const ports = { get: cannedGet(jsonText), log: settings.log };
+    
+    const results = await stops(ports, { enableDebug }, '5515');
 
-    expect(result.name).to.equal('Wilton Road at Warwick Street');
-    expect(result.sms) .to.equal('4130');
+    const result = results[0];
+
+    expect(result.name).to.equal('Manners Street at Cuba Street - Stop A');
+    expect(result.sms) .to.equal('5515');
   });
 });
