26d25117975a6806314a23fc5c15563c264a96e7
ben.biddington <ben.biddington@gmail.com>
2019-03-27 14:00:51 +1300

    Refactor: duplication

--- src/adapters/metlink-stops-view.js
+++ src/adapters/metlink-stops-view.js
@@ -1,15 +1,14 @@
+const fs        = require('fs');
+const { promisify } = require('util');
+const readFile      = promisify(fs.readFile);
+const writeFile     = promisify(fs.writeFile);
+const exists        = promisify(fs.exists);
+
 const fileName = "./.stops"; 
 
 const updateStops = (log, stopNumber) => {
-    const fs        = require('fs');
-  
-    const { promisify } = require('util');
-    const readFile      = promisify(fs.readFile);
-    const writeFile     = promisify(fs.writeFile);
-    const exists        = promisify(fs.exists);
-    
-    const read  = ()    => readFile (fileName,       'utf8');
-    const write = text  => writeFile(fileName, text, 'utf8');
+    const read          = ()    => readFile (fileName,       'utf8');
+    const write         = text  => writeFile(fileName, text, 'utf8');
 
     return exists(fileName).
       then(fileExists => {
@@ -31,21 +30,15 @@ const updateStops = (log, stopNumber) => {
       then(write);
   }
   
-  const listStops = log => {
-    const fs        = require('fs');
-  
-    const { promisify } = require('util');
-    const readFile      = promisify(fs.readFile);
-    const exists        = promisify(fs.exists);
-  
-    return exists(fileName).
-      then(fileExists => {
-        if (fileExists === true)
-          return readFile(fileName, "utf8");
+const listStops = log => {
+  return exists(fileName).
+    then(fileExists => {
+      if (fileExists === true)
+        return readFile(fileName, "utf8");
          
         return Promise.resolve('No stops on file');
       }).
-      then(log);
+    then(log);
   }
 
   module.exports.updateStops = updateStops;
