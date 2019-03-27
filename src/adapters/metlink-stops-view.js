84ca0472c87cc19018031ba163d3fab94a57f4e4
Ben Biddington <ben.biddington@gmail.com>
2019-03-27 14:03:45 +1300

    Formatting

--- src/adapters/metlink-stops-view.js
+++ src/adapters/metlink-stops-view.js
@@ -1,45 +1,45 @@
-const fs        = require('fs');
+const fs = require('fs');
 const { promisify } = require('util');
-const readFile      = promisify(fs.readFile);
-const writeFile     = promisify(fs.writeFile);
-const exists        = promisify(fs.exists);
+const readFile = promisify(fs.readFile);
+const writeFile = promisify(fs.writeFile);
+const exists = promisify(fs.exists);
 
-const fileName = "./.stops"; 
+const fileName = "./.stops";
 
 const updateStops = (log, stopNumber) => {
-    const read          = ()    => readFile (fileName,       'utf8');
-    const write         = text  => writeFile(fileName, text, 'utf8');
+  const read = () => readFile(fileName, 'utf8');
+  const write = text => writeFile(fileName, text, 'utf8');
 
-    return exists(fileName).
-      then(fileExists => {
-        if (false === fileExists)
-          return write('');
-         
-        return Promise.resolve();
-      }).
-      then(read).
-      then(text => {
-        const stops = text.split('\n').filter(line => line.length > 0);
-        
-        if (false === stops.includes(stopNumber)) {
-          stops.push(stopNumber);
-        }
+  return exists(fileName).
+    then(fileExists => {
+      if (false === fileExists)
+        return write('');
 
-        return stops.join('\n');
-      }).
-      then(write);
-  }
-  
+      return Promise.resolve();
+    }).
+    then(read).
+    then(text => {
+      const stops = text.split('\n').filter(line => line.length > 0);
+
+      if (false === stops.includes(stopNumber)) {
+        stops.push(stopNumber);
+      }
+
+      return stops.join('\n');
+    }).
+    then(write);
+}
+
 const listStops = log => {
   return exists(fileName).
     then(fileExists => {
       if (fileExists === true)
         return readFile(fileName, "utf8");
-         
-        return Promise.resolve('No stops on file');
-      }).
-    then(log);
-  }
 
-  module.exports.updateStops = updateStops;
-  module.exports.listStops = listStops;
+      return Promise.resolve('No stops on file');
+    }).
+    then(log);
+}
+
+module.exports.updateStops = updateStops;
+module.exports.listStops = listStops;
