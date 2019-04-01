328b4dea64f64f48fc61ea5cf6a9a3a7c9e0fe0c
ben.biddington <ben.biddington@gmail.com>
2019-04-01 20:02:07 +1300

    [DRAFT] Make a seen database

--- src/adapters/cli/hacker-news/hn-cli.js
+++ src/adapters/cli/hacker-news/hn-cli.js
@@ -40,4 +40,33 @@ program.
     });
   });
 
+program.
+  version('0.0.1').
+  command("hide [id]").
+  option("-v --verbose"                   , "Enable verbose logging").
+  option("-t --trace"                     , "Enable trace logging").
+  option("-l --logLabels <logLabels...>"  , "Log labels", []).
+  action(async (id, opts) => {
+    debug         = (process.env.DEBUG == 1 || opts.verbose === true) 
+      ? (m, label = null) => {
+        if (opts.logLabels.length === 0 || opts.logLabels.includes(label)) {
+          const prefix = label ? `[DEBUG, ${label}]` : '[DEBUG]';
+
+          fs.writeSync(1, `${prefix} ${m}\n`);
+        }
+      }
+      : () => {};
+
+    // [i] http://lokijs.org/#/
+    const loki = require('lokijs');
+
+    var db = new loki('loki.json');
+
+    const seen = db.addCollection('children');
+
+    seen.insert({ id });
+
+    log(`There are <${seen.count()}> seen items`);
+  });
+
 program.parse(process.argv);
