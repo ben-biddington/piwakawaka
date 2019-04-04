c73467f3baa462ececc0150a05990305535725e0
Ben Biddington <ben.biddington@xero.com>
2019-04-05 10:59:46 +1300

    Refactor: duplication

--- src/adapters/hn.js
+++ src/adapters/hn.js
@@ -1,8 +1,12 @@
 const { timeAsync } = require('../core/time');
 
-const fs    = require('fs');
-const util  = require('util');
-const path  = require('path');
+const fs        = require('fs');
+const util      = require('util');
+const path      = require('path');
+const write     = util.promisify(fs.writeFile);
+const exists    = util.promisify(fs.exists);
+const mkdir     = util.promisify(fs.mkdir);
+const read      = util.promisify(fs.readFile);
 
 class DiskCache {
   constructor(dir) {
@@ -10,21 +14,24 @@ class DiskCache {
   }
 
   async set(name, what) {
-    const fullPath  = path.join(this._dir, name.toString());
-    const write     = util.promisify(fs.writeFile);
-    
-    return write(fullPath, JSON.stringify(what));
+    return write(this.fullPath(name), JSON.stringify(what));
   }
+
+  async ensureDir() {
+    return exists(this._dir).then(okay => okay === true ? Promise.resolve() : mkdir(this._dir));
+  };
 
   async get(name) {
-    const fullPath = path.join(this._dir, name.toString());
-    const exists   = util.promisify(fs.exists);
-    const read     = util.promisify(fs.readFile);
+    const fullFilePath = this.fullPath(name);
 
-    return exists(fullPath).then(yes => {
-      return yes ? read(fullPath, 'utf8').then(JSON.parse) : Promise.resolve(null);
-    });
+    return this.ensureDir().
+      then(()   => exists(fullFilePath)).
+      then(yes  => yes ? read(fullFilePath, 'utf8').then(JSON.parse) : Promise.resolve());
   }
+
+  fullPath(name) {
+    return path.join(this._dir, name.toString())
+  };
 }
 
 const mapItem = item => { 
