4cb9519e0e24a9d6e19458aeab370ad15578743a
ben.biddington <ben.biddington@gmail.com>
2019-04-09 09:13:51 +1200

    Auto-apply schema

--- test/integration.tests/hacker-news/amazon/connection-examples.js
+++ test/integration.tests/hacker-news/amazon/connection-examples.js
@@ -7,7 +7,80 @@ const config = exists(filename) ? JSON.parse(read(filename)) : null;
 
 const check = config ? it : (name) => xit(`'${name}' -- skipped because the <${filename}> file does not exist`);
 
+class Database {
+  constructor(config) {
+    this._config = config;
+    this._connection = null;
+  }
+
+  async applySchema() {
+    const sql = 'CREATE TABLE IF NOT EXISTS seen (id INT, timestamp DATE, PRIMARY KEY (id))';
+    
+    await this.connect();
+
+    return new Promise((accept, reject) => {
+      this._connection.query(sql, (error, results, fields) => {
+        if (error){
+          reject(error);
+          return;
+        }
+
+        accept(results);
+      });
+    });
+  }
+
+  async listSeen() {
+    await this.connect();
+
+    return new Promise((accept, reject) => {
+      this._connection.query('SELECT id from seen', (error, results, fields) => {
+        if (error){
+          reject(error);
+          return;
+        }
+
+        accept(results);
+      });
+    });
+  }
+
+  async connect() {
+    const mysql      = require('mysql');
+    
+    this._connection = await mysql.createConnection(config, error => {
+      if (error)
+        throw error;
+    });
+
+    await new Promise((accept, reject) => {
+      this._connection.connect(e => {
+        if (e) {
+          reject(e);
+          return;
+        }
+
+        accept();
+      });
+    });  
+  }
+
+  async close() {
+    await this._connection.end();
+  }
+}
+
 describe('Connecting to mysql database', async () => {
+  check('can list all seen items', async () => {
+    const database  = new Database(config);
+    
+    await database.applySchema();
+
+    const seen      = await database.listSeen().finally(() => database.close());
+
+    expect(seen.length).to.equal(0);
+  });
+
   check('allows connection', async () => {
     let connection = null;
     
