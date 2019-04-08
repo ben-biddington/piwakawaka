e5d328537798bcfe74e48f4c52689cbe8ab0e711
ben.biddington <ben.biddington@gmail.com>
2019-04-09 09:36:38 +1200

    Delete learning test

--- test/integration.tests/hacker-news/amazon/connection-examples.js
+++ test/integration.tests/hacker-news/amazon/connection-examples.js
@@ -107,49 +107,8 @@ describe('Connecting to mysql database', async () => {
       return database.applySchema().
         then(   () => database.removeSeen(1337)).
         then(   () => database.addSeen(1337)).
-        then(   () => database.listSeen())
-    });
+        then(   () => database.listSeen()) });
 
     expect(seen.map(it => it.id)).to.contain(1337);
-  });
-
-  check('allows connection', async () => {
-    let connection = null;
-    
-    try {
-      const mysql      = require('mysql');
-      
-      connection = await mysql.createConnection(config, error => {
-        if (error)
-          throw error;
-      });
- 
-      await new Promise((accept, reject) => {
-        connection.connect(e => {
-          if (e) {
-            reject(e);
-            return;
-          }
-
-          accept();
-        });
-      });
-
-      const result = await new Promise((accept, reject) => {
-        connection.query('SELECT 1 as result', (error, results, fields) => {
-          if (error){
-            reject(error);
-            return;
-          }
-
-          accept(results);
-        });
-      });
-
-      expect(result[0].result).to.equal(1);
- 
-    } finally {
-      connection.end();
-    }
   });
 });
