468661f9391caadd9292b0e66a6352b881491b7f
ben.biddington <ben.biddington@gmail.com>
2019-04-10 19:13:04 +1200

    Refactor: simplify

--- src/core/time.js
+++ src/core/time.js
@@ -1,16 +1,7 @@
 const timeAsync = async fn => {
-  let result;
-  
   const start = new Date();
-  let finish;
 
-  try { 
-    result = await fn(); 
-  } finally {
-    finish = new Date();
-  }
-
-  return { duration: (finish - start), result };
+  return fn().then(result => ({ duration: (new Date() - start), result }));
 }
 
 module.exports.timeAsync = timeAsync;
