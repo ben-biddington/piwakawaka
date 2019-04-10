db3474c67ba90eb2ba772c8afd317f126c0b18fd
ben.biddington <ben.biddington@gmail.com>
2019-04-10 15:23:38 +1200

    Formatting

--- src/adapters/adapters.js
+++ src/adapters/adapters.js
@@ -2,10 +2,9 @@ const { Storage : LocalStorage }  = require('../adapters/local/storage');
 const { realTime }                = require('./metlink');
 const { top }                     = require('../adapters/hn');
 
-const log = m => console.log(`[LOG.ADAPTER] ${m}`);
+const log             = m => console.log(`[LOG.ADAPTER] ${m}`);
 const newLocalStorage = () => new LocalStorage();
-
-const get = (url, headers) => fetch(url).
-  then(reply => reply.text().then(body => ({ statusCode: reply.status, body })));
+const get             = (url, headers) 
+  => fetch(url).then(reply => reply.text().then(body => ({ statusCode: reply.status, body })));
 
 module.exports = { get, log, newLocalStorage, realTime, top }
